a4020ea0233a95e9a08abbcad75d25633c2ad929
Nilesh Shrestha <nileshshr97@gmail.com>
2022-11-27 18:16:31 +0545

    added footer to product page

--- pages/Products.jsx
+++ pages/Products.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
-import Navigation from '../components/Navigation'
-
+import React from "react";
+import Footer from "../components/Footer";
+import Footerbottom from "../components/Footerbottom";
+import Navigation from "../components/Navigation";
 
 function Products() {
   return (
     <>
-        <Navigation/>
+      <Navigation />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem architecto
+      veniam odio officia, placeat cumque blanditiis iusto numquam corporis sit
+      velit reprehenderit, consequuntur quaerat doloremque nihil maiores.
+      Accusantium, eum. Sit natus, iure eaque aliquid consequatur quos, culpa
+      asperiores fugit placeat aut dolor explicabo perferendis laborum commodi
+      sapiente quisquam atque tenetur. Lorem ipsum dolor sit amet consectetur,
+      adipisicing elit. Expedita officiis impedit minima atque perferendis,
+      provident ullam sit tempore id soluta. Inventore nam id dicta est ad
+      ratione labore quia laudantium amet distinctio. Alias, sit iusto facere
+      quos nesciunt quidem reiciendis nostrum adipisci? Ut rem quidem temporibus
+      a porro, exercitationem quos! Aut animi veritatis amet vitae nesciunt quae
+      ab ratione natus earum fuga esse similique quia magnam eligendi, pariatur
+      iste qui laboriosam rem incidunt debitis accusantium voluptatum. Qui fugit
+      ea dolore animi ipsum dolorem. Reiciendis exercitationem id provident.
+      Quae eaque corrupti hic, architecto maxime eos esse. Iure ullam molestiae
+      quae veniam ex dolores, id cupiditate tenetur itaque quisquam provident
+      inventore corrupti nam, deleniti, dicta minima? Qui itaque quis ratione
+      sapiente assumenda, expedita doloremque ea debitis quaerat illo! Ex,
+      quaerat repellat. Ex cupiditate, aperiam delectus laborum, labore
+      laboriosam cumque in eum eveniet quasi explicabo ipsum eos, pariatur
+      molestias. Nulla, error fugiat veritatis dolores libero consectetur quasi
+      veniam reiciendis ipsum quibusdam, inventore officiis cum labore dolore
+      alias tenetur vel quia quisquam delectus incidunt nobis? Provident magni
+      impedit ullam harum aliquid perspiciatis, optio quos incidunt adipisci.
+      Nesciunt dolore adipisci assumenda labore eos nostrum voluptas. Quasi a
+      nostrum possimus ipsa doloribus pariatur, fugit eveniet recusandae.
+      <Footer />
+      <Footerbottom />
     </>
-  )
+  );
 }
 
-export default Products
+export default Products;
